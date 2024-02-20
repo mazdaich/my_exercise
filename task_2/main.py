a660d4c2643ee03b618a64df29289f3318591c4c
sergey kuznetsov <mazdai.88@mail.ru>
2024-02-20 21:53:17 +0300

    создана задача 2

---
+++ task_2/main.py
@@ -0,0 +1,34 @@
+class ArithmeticProgression:
+    def __init__(self, start, step):
+        self.start = start
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += self.step
+        return self.start - self.step
+
+
+class GeometricProgression:
+    def __init__(self, start, step):
+        self.start = start
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        r = self.start
+        self.start *= self.step
+        return r
+
+
+if __name__ == '__main__':
+    progression = ArithmeticProgression(0, 1)
+
+    for elem in progression:
+        if elem > 10:
+            break
+        print(elem, end=' ')
